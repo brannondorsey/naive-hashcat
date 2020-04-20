7693ed5c92868c011d3b40d711e53a74d571af7e
full <fullbl@mail.com>
2020-04-20 16:57:04 +0200

    fix old hashcat addresses

--- naive-hashcat.sh
+++ naive-hashcat.sh
@@ -42,19 +42,19 @@ fi
 
 # DICTIONARY ATTACK WITH RULES------------------------------------------------------------
 # now lets move on to a rule based attack, d3ad0ne.rule is a great one to start with
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/d3ad0ne.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat/rules/d3ad0ne.rule --potfile-path "$POT_FILE"
 
 # rockyou is pretty good, and not too slow
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/rockyou-30000.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat/rules/rockyou-30000.rule --potfile-path "$POT_FILE"
 
 
 # MEDIUM
 # dive is a great rule file, but it takes a bit longer to run, so we will run it after d3ad0ne and rockyou
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/dive.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat/rules/dive.rule --potfile-path "$POT_FILE"
 
 # HEAVY
 # MASK ATTACK (BRUTE-FORCE)---------------------------------------------------------------
-"$HASHCAT" -m "$HASH_TYPE" -a 3 "$HASH_FILE" hashcat-3.6.0/masks/rockyou-1-60.hcmask --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 3 "$HASH_FILE" hashcat/masks/rockyou-1-60.hcmask --potfile-path "$POT_FILE"
 
 # COMBINATION ATTACK----------------------------------------------------------------------
 # this one can take 12+ hours, don't use it by default
